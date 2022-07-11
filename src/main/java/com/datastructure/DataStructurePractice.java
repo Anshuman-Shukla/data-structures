458c93ea433955ac9866106005adb8150b160865
Anshuman Shukla <anshuman.shukla@knoldus.com>
2022-07-11 23:35:05 +0530

    Added the basic structure

---
+++ src/main/java/com/datastructure/DataStructurePractice.java
@@ -0,0 +1,13 @@
+package com.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructurePractice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructurePractice.class, args);
+	}
+
+}
